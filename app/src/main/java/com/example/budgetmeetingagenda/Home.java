d9456bf1daf0616808bbcf4010c0b9d386039297
unknown <2015wchoi@2300D0097749572.local.tjhsst.edu>
2014-10-09 13:52:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/budgetmeetingagenda/Home.java
@@ -0,0 +1,72 @@
+package com.example.budgetmeetingagenda;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ImageView logo = (ImageView)findViewById(R.id.imageView);
+        Button button_01 = (Button)findViewById(R.id.button);
+        Button button_02 = (Button)findViewById(R.id.button2);
+        Button button_03 = (Button)findViewById(R.id.button3);
+        Button button_04 = (Button)findViewById(R.id.button4);
+
+        logo.setImageResource(R.drawable.spoderman);
+        button_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Sport1.class));
+            }
+        });
+        button_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Sport2.class));
+            }
+        });
+        button_03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Sport3.class));
+            }
+        });
+        button_04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EditInfo.class));
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
