2d381d961e2d57be586a90e21e51940d17f10662
Anderseus <anders.wk.choi@gmail.com>
2014-10-23 13:42:43 -0400

    Added home button to Sensors page

--- app/src/main/java/com/example/budgetmeetingagenda/Sensors.java
+++ app/src/main/java/com/example/budgetmeetingagenda/Sensors.java
@@ -1,6 +1,8 @@
 package com.example.budgetmeetingagenda;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,6 +11,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -76,6 +80,16 @@ public class Sensors extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sensors.this, Home.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
